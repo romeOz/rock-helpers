a9b2e3f98308358366e9217e956cbed924ddca05
romeOz <serggalka@gmail.com>
2015-01-29 11:52:30 +0300

    Simple refactoring.

--- src/Pagination.php
+++ src/Pagination.php
@@ -42,120 +42,19 @@ class Pagination
         if (empty($count) || !is_numeric($count) || !is_numeric($pageLimit)) {
             return [];
         }
-        if (empty($pageVar) || !is_string($pageVar)) {
-            $pageVar = 'page';
-        }
-        $result['pageVar'] = $pageVar;
+
         $count = (int)$count;
         $pageLimit = (int)$pageLimit;
         $pageCount = intval(($count - 1) / $limit) + 1;
-        $result['pageCount'] = $pageCount;
+        $result = [
+            'pageCount' => $pageCount
+        ];
         // DESC
         if ($sort === SORT_DESC) {
-            if (!isset($pageCurrent)) {
-                $pageCurrent = $pageCount;
-            }
-            if ($pageCurrent < 1) {
-                $pageCurrent = $pageCount;
-            } elseif ($pageCurrent > $pageCount) {
-                $pageCurrent = $pageCount;
-            }
-            $result['pageCurrent'] = $pageCurrent;
-            // if count of pages is less, than the limit
-            if ($pageCount >= $pageLimit) {
-                $pageStart = $pageCurrent + floor(($pageLimit - 1) / 2);
-                if ($pageStart > $pageCount) {
-                    $pageStart = $pageCount;
-                }
-                $pageEnd = $pageStart - $pageLimit + 1;
-                if ($pageEnd <= 1) {
-                    $pageStart = $pageLimit;
-                    $pageEnd = 1;
-                }
-            } else {
-                $pageStart = $pageCount;
-                $pageEnd = 1;
-            }
-            $result['pageStart'] = (int)$pageStart;
-            $result['pageEnd'] = (int)$pageEnd;
-            for ($i = $pageStart; $i >= $pageEnd; --$i) {
-                $result['pageDisplay'][] = $i;
-                if ($i === $result['pageCurrent']) {
-                    if (!(($i + 1) > $pageStart)) {
-                        $result['pagePrev'] = $i + 1;
-                    }
-                    if (!(($i - 1) < $pageEnd)) {
-                        $result['pageNext'] = $i - 1;
-                    }
-                }
-            }
-            // page first number
-            if ($pageCurrent < $pageCount) {
-                $result['pageFirst'] = $pageCount;
-            } else {
-                $result['pageFirst'] = null;
-            }
-            // page last number
-            if ($pageCurrent > 1) {
-                $result['pageLast'] = 1;
-            } else {
-                $result['pageLast'] = null;
-            }
-            $result['offset'] = ($pageCount - $pageCurrent) * $limit;
-            $result['limit'] = $limit;
-            // ASC
+            static::sortDESC($result, $pageCurrent, $pageCount, $pageLimit, $limit);
+        // ASC
         } else {
-            if (!isset($pageCurrent)) {
-                $pageCurrent = 1;
-            }
-            if ($pageCurrent < 1) {
-                $pageCurrent = 1;
-            } elseif ($pageCurrent > $pageCount) {
-                $pageCurrent = $pageCount;
-            }
-            $result['pageCurrent'] = $pageCurrent;
-            // if count of pages is less, than the limit
-            if ($pageCount >= $pageLimit) {
-                $pageStart = $pageCurrent - floor(($pageLimit - 1) / 2);
-                if ($pageStart < 1) {
-                    $pageStart = 1;
-                }
-                $pageEnd = $pageStart + $pageLimit - 1;
-                if ($pageEnd > $pageCount) {
-                    $pageEnd = $pageCount;
-                    $pageStart = $pageEnd - $pageLimit + 1;
-                }
-            } else {
-                $pageStart = 1;
-                $pageEnd = $pageCount;
-            }
-            $result['pageStart'] = (int)$pageStart;
-            $result['pageEnd'] = (int)$pageEnd;
-            for ($i = $pageStart; $i <= $pageEnd; ++$i) {
-                $result['pageDisplay'][] = $i;
-                if ($i === $result['pageCurrent']) {
-                    if (!(($i - 1) < $pageStart)) {
-                        $result['pagePrev'] = $i - 1;
-                    }
-                    if (!(($i + 1) > $pageEnd)) {
-                        $result['pageNext'] = $i + 1;
-                    }
-                }
-            }
-            // page first number
-            if ($pageCurrent > 1) {
-                $result['pageFirst'] = 1;
-            } else {
-                $result['pageFirst'] = null;
-            }
-            // page last number
-            if ($pageCurrent < $pageCount) {
-                $result['pageLast'] = $pageCount;
-            } else {
-                $result['pageLast'] = null;
-            }
-            $result['offset'] = ($pageCurrent - 1) * $limit;
-            $result['limit'] = $limit;
+            static::sortASC($result, $pageCurrent, $pageCount, $pageLimit, $limit);
         }
         // Count items of more
         $result['countMore'] = $count - ($result['offset'] + $result['limit']);
@@ -165,5 +64,115 @@ class Pagination
                 : 0;
 
         return $result;
+    }
+
+    protected static function sortDESC(array &$result, $pageCurrent, $pageCount, $pageLimit, $limit)
+    {
+        if (!isset($pageCurrent)) {
+            $pageCurrent = $pageCount;
+        }
+        if ($pageCurrent < 1) {
+            $pageCurrent = $pageCount;
+        } elseif ($pageCurrent > $pageCount) {
+            $pageCurrent = $pageCount;
+        }
+        $result['pageCurrent'] = $pageCurrent;
+        // if count of pages is less, than the limit
+        if ($pageCount >= $pageLimit) {
+            $pageStart = $pageCurrent + floor(($pageLimit - 1) / 2);
+            if ($pageStart > $pageCount) {
+                $pageStart = $pageCount;
+            }
+            $pageEnd = $pageStart - $pageLimit + 1;
+            if ($pageEnd <= 1) {
+                $pageStart = $pageLimit;
+                $pageEnd = 1;
+            }
+        } else {
+            $pageStart = $pageCount;
+            $pageEnd = 1;
+        }
+        $result['pageStart'] = (int)$pageStart;
+        $result['pageEnd'] = (int)$pageEnd;
+        for ($i = $pageStart; $i >= $pageEnd; --$i) {
+            $result['pageDisplay'][] = $i;
+            if ($i === $result['pageCurrent']) {
+                if (!(($i + 1) > $pageStart)) {
+                    $result['pagePrev'] = $i + 1;
+                }
+                if (!(($i - 1) < $pageEnd)) {
+                    $result['pageNext'] = $i - 1;
+                }
+            }
+        }
+        // page first number
+        if ($pageCurrent < $pageCount) {
+            $result['pageFirst'] = $pageCount;
+        } else {
+            $result['pageFirst'] = null;
+        }
+        // page last number
+        if ($pageCurrent > 1) {
+            $result['pageLast'] = 1;
+        } else {
+            $result['pageLast'] = null;
+        }
+        $result['offset'] = ($pageCount - $pageCurrent) * $limit;
+        $result['limit'] = $limit;
+    }
+
+    protected static function sortASC(array &$result, $pageCurrent, $pageCount, $pageLimit, $limit)
+    {
+        if (!isset($pageCurrent)) {
+            $pageCurrent = 1;
+        }
+        if ($pageCurrent < 1) {
+            $pageCurrent = 1;
+        } elseif ($pageCurrent > $pageCount) {
+            $pageCurrent = $pageCount;
+        }
+        $result['pageCurrent'] = $pageCurrent;
+        // if count of pages is less, than the limit
+        if ($pageCount >= $pageLimit) {
+            $pageStart = $pageCurrent - floor(($pageLimit - 1) / 2);
+            if ($pageStart < 1) {
+                $pageStart = 1;
+            }
+            $pageEnd = $pageStart + $pageLimit - 1;
+            if ($pageEnd > $pageCount) {
+                $pageEnd = $pageCount;
+                $pageStart = $pageEnd - $pageLimit + 1;
+            }
+        } else {
+            $pageStart = 1;
+            $pageEnd = $pageCount;
+        }
+        $result['pageStart'] = (int)$pageStart;
+        $result['pageEnd'] = (int)$pageEnd;
+        for ($i = $pageStart; $i <= $pageEnd; ++$i) {
+            $result['pageDisplay'][] = $i;
+            if ($i === $result['pageCurrent']) {
+                if (!(($i - 1) < $pageStart)) {
+                    $result['pagePrev'] = $i - 1;
+                }
+                if (!(($i + 1) > $pageEnd)) {
+                    $result['pageNext'] = $i + 1;
+                }
+            }
+        }
+        // page first number
+        if ($pageCurrent > 1) {
+            $result['pageFirst'] = 1;
+        } else {
+            $result['pageFirst'] = null;
+        }
+        // page last number
+        if ($pageCurrent < $pageCount) {
+            $result['pageLast'] = $pageCount;
+        } else {
+            $result['pageLast'] = null;
+        }
+        $result['offset'] = ($pageCurrent - 1) * $limit;
+        $result['limit'] = $limit;
     }
 }
--- tests/PaginationTest.php
+++ tests/PaginationTest.php
@@ -9,40 +9,38 @@ class PaginationTest extends \PHPUnit_Framework_TestCase
     public function testGetAsSortASC()
     {
         // count "0"
-        $this->assertSame(Pagination::get(0), []);
+        $this->assertSame([], Pagination::get(0));
 
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 1,
             'pageCurrent' => 1,
             'pageStart' => 1,
             'pageEnd' => 1,
             'pageDisplay' =>
-                array (
+                [
                     0 => 1,
-                ),
+                ],
             'pageFirst' => NULL,
             'pageLast' => NULL,
             'offset' => 0,
             'limit' => 10,
             'countMore' => 0,
-        );
-        $this->assertSame(Pagination::get(7,1),$actual);
+        ];
+        $this->assertSame($expected, Pagination::get(7,1));
 
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 5,
             'pageCurrent' => 2,
             'pageStart' => 1,
             'pageEnd' => 5,
             'pageDisplay' =>
-                array (
+                [
                     0 => 1,
                     1 => 2,
                     2 => 3,
                     3 => 4,
                     4 => 5,
-                ),
+                ],
             'pagePrev' => 1,
             'pageNext' => 3,
             'pageFirst' => 1,
@@ -50,117 +48,112 @@ class PaginationTest extends \PHPUnit_Framework_TestCase
             'offset' => 10,
             'limit' => 10,
             'countMore' => 30,
-        );
-        $this->assertSame(Pagination::get(50, 2), $actual);
+        ];
+        $this->assertSame($expected, Pagination::get(50, 2));
 
         // first page
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 2,
             'pageCurrent' => 1,
             'pageStart' => 1,
             'pageEnd' => 2,
             'pageDisplay' =>
-                array (
+                [
                     0 => 1,
                     1 => 2,
-                ),
+                ],
             'pageNext' => 2,
             'pageFirst' => NULL,
             'pageLast' => 2,
             'offset' => 0,
             'limit' => 5,
             'countMore' => 2,
-        );
-        $this->assertSame(Pagination::get(7,null,5), $actual);
-        $this->assertSame(Pagination::get(7,0,5), $actual);
-        $this->assertSame(Pagination::get(7,-1,5), $actual);
-        $this->assertSame(Pagination::get(7,'foo',5), $actual);
+        ];
+        $this->assertSame($expected, Pagination::get(7,null,5));
+        $this->assertSame($expected, Pagination::get(7,0,5));
+        $this->assertSame($expected, Pagination::get(7,-1,5));
+        $this->assertSame($expected, Pagination::get(7,'foo',5));
 
         // next page
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 2,
             'pageCurrent' => 1,
             'pageStart' => 1,
             'pageEnd' => 2,
             'pageDisplay' =>
-                array (
+                [
                     0 => 1,
                     1 => 2,
-                ),
+                ],
             'pageNext' => 2,
             'pageFirst' => NULL,
             'pageLast' => 2,
             'offset' => 0,
             'limit' => 5,
             'countMore' => 2,
-        );
-        $this->assertSame(Pagination::get(7, 1, 5), $actual);
+        ];
+        $this->assertSame($expected, Pagination::get(7, 1, 5));
 
         // page last
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 2,
             'pageCurrent' => 2,
             'pageStart' => 1,
             'pageEnd' => 2,
             'pageDisplay' =>
-                array (
+                [
                     0 => 1,
                     1 => 2,
-                ),
+                ],
             'pagePrev' => 1,
             'pageFirst' => 1,
             'pageLast' => NULL,
             'offset' => 5,
             'limit' => 5,
             'countMore' => 0,
-        );
-        $this->assertSame(Pagination::get(7, 7, 5), $actual);
+        ];
+        $this->assertSame($expected, Pagination::get(7, 7, 5));
 
     }
 
     public function testGetAsSortDESC()
     {
         // first page
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 2,
             'pageCurrent' => 2,
             'pageStart' => 2,
             'pageEnd' => 1,
             'pageDisplay' =>
-                array (
+                [
                     0 => 2,
                     1 => 1,
-                ),
+                ],
             'pageNext' => 1,
             'pageFirst' => NULL,
             'pageLast' => 1,
             'offset' => 0,
             'limit' => 5,
             'countMore' => 2,
-        );
-        $this->assertSame(Pagination::get(7, null, 5, SORT_DESC), $actual);
-        $this->assertSame(Pagination::get(7, 0, 5, SORT_DESC), $actual);
-        $this->assertSame(Pagination::get(7, -1, 5, SORT_DESC), $actual);
-        $this->assertSame(Pagination::get(7, 'foo', 5, SORT_DESC), $actual);
+        ];
+        $this->assertSame($expected, Pagination::get(7, null, 5, SORT_DESC));
+        $this->assertSame($expected, Pagination::get(7, 0, 5, SORT_DESC));
+        $this->assertSame($expected, Pagination::get(7, -1, 5, SORT_DESC));
+        $this->assertSame($expected, Pagination::get(7, 'foo', 5, SORT_DESC));
 
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 5,
             'pageCurrent' => 2,
             'pageStart' => 5,
             'pageEnd' => 1,
             'pageDisplay' =>
-                array (
+                [
                     0 => 5,
                     1 => 4,
                     2 => 3,
                     3 => 2,
                     4 => 1,
-                ),
+                ],
             'pagePrev' => 3,
             'pageNext' => 1,
             'pageFirst' => 5,
@@ -168,49 +161,47 @@ class PaginationTest extends \PHPUnit_Framework_TestCase
             'offset' => 30,
             'limit' => 10,
             'countMore' => 10,
-        );
-        $this->assertSame(Pagination::get(50, 2, 10, SORT_DESC), $actual);
+        ];
+        $this->assertSame($expected, Pagination::get(50, 2, 10, SORT_DESC));
 
         // next page
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 2,
             'pageCurrent' => 2,
             'pageStart' => 2,
             'pageEnd' => 1,
             'pageDisplay' =>
-                array (
+                [
                     0 => 2,
                     1 => 1,
-                ),
+                ],
             'pageNext' => 1,
             'pageFirst' => NULL,
             'pageLast' => 1,
             'offset' => 0,
             'limit' => 5,
             'countMore' => 2,
-        );
-        $this->assertSame(Pagination::get(7, 6, 5, SORT_DESC), $actual);
+        ];
+        $this->assertSame($expected, Pagination::get(7, 6, 5, SORT_DESC));
 
         // last page
-        $actual = array (
-            'pageVar' => 'page',
+        $expected = [
             'pageCount' => 2,
             'pageCurrent' => 1,
             'pageStart' => 2,
             'pageEnd' => 1,
             'pageDisplay' =>
-                array (
+                [
                     0 => 2,
                     1 => 1,
-                ),
+                ],
             'pagePrev' => 2,
             'pageFirst' => 2,
             'pageLast' => NULL,
             'offset' => 5,
             'limit' => 5,
             'countMore' => 0,
-        );
-        $this->assertSame(Pagination::get(7, 1, 5, SORT_DESC), $actual);
+        ];
+        $this->assertSame($expected, Pagination::get(7, 1, 5, SORT_DESC));
     }
 }
