1dec12a829d2c4d5d6332388a1082f5e1bf4d714
romeOz <serggalka@gmail.com>
2015-02-16 15:45:11 +0300

    Rename constant `Pagination::PAGE_VAR` to `PAGE_ARG_URL`.

--- src/Pagination.php
+++ src/Pagination.php
@@ -16,7 +16,7 @@ class Pagination
     const SORT = SORT_ASC;
     const LIMIT = 10;
     const PAGE_LIMIT = 5;
-    const PAGE_VAR = 'page';
+    const PAGE_ARG_URL = 'page';
 
     /**
      * Get array of pages.
