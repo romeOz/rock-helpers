be9932e369d76042adc3c78b906996624ace0457
romeOz <serggalka@gmail.com>
2015-03-17 19:30:43 +0300

    Added `Link` helper.

---
+++ tests/LinkTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace rockunit;
+
+
+use rock\helpers\Link;
+
+class LinkTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $excpected = [
+            'self' =>
+                [
+                    'href' => 'http://site/',
+                ],
+            'first' =>
+                [
+                    'href' => 'http://site/',
+                ],
+            'prev' =>
+                [
+                    'href' => 'http://site/',
+                ],
+            'next' =>
+                [
+                    'href' => 'http://site/?page=48',
+                ],
+            'last' =>
+                [
+                    'href' => 'http://site/?page=1',
+                ],
+        ];
+        $this->assertSame($excpected, Link::serialize([
+            Link::REL_SELF => 'http://site/',
+            'first' => 'http://site/',
+            'prev'=>'http://site/',
+            'next' =>'http://site/?page=48',
+            'last' => 'http://site/?page=1',
+        ]));
+    }
+}
