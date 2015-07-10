a4fcd5025b3a3d2bccb9446bd859766b5503bd14
romeOz <serggalka@gmail.com>
2015-07-10 23:56:56 +0300

    Refactoring.

--- src/Instance.php
+++ src/Instance.php
@@ -40,12 +40,34 @@ class Instance
                 }
                 return null;
             }
-            if (!empty($args)) {
-                $reflect = new \ReflectionClass($defaultClass);
-                $args = array_merge($args, $config ? [$config] : $config);
-                return $reflect->newInstanceArgs($reflect->getConstructor() ? $args : []);
-            }
-            return new $defaultClass($config);
+
+            $reflect = new \ReflectionClass($defaultClass);
+            $args = static::calculateArgs($reflect, $args, $config);
+            return $reflect->newInstanceArgs($reflect->getConstructor() ? $args : []);
         }
+    }
+
+    protected static function calculateArgs(\ReflectionClass $reflect, array $args = [], array $config = [])
+    {
+        $constructor = $reflect->getConstructor();
+        if ($constructor instanceof \ReflectionMethod && ($params = $constructor->getParameters())) {
+            $i = 0;
+            foreach ($params as $param) {
+                if (!array_key_exists($i, $args) && $param->isDefaultValueAvailable()) {
+                    $args[$i] = $param->getDefaultValue();
+                }
+                ++$i;
+            }
+
+            $last = end($params);
+            $interfaces = array_flip($reflect->getInterfaceNames());
+            if (isset($interfaces['rock\base\ObjectInterface']) && is_array($last->getDefaultValue())) {
+                end($args);
+                $args[key($args)] = $config;
+                reset($args);
+            }
+        }
+
+        return $args;
     }
 }
--- tests/InstanceTest.php
+++ tests/InstanceTest.php
@@ -3,6 +3,7 @@
 namespace rockunit;
 
 
+use rock\base\ObjectInterface;
 use rock\base\ObjectTrait;
 use rock\helpers\Instance;
 use rock\helpers\InstanceException;
@@ -29,9 +30,19 @@ class InstanceTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('foo test', $instance->foo);
         $this->assertSame('bar test', $instance->bar);
 
-        $instance = Instance::ensure(['class' => InstanceArgs::className(), 'foo' => 'foo test'], InstanceArgs::className(), ['bar test']);
+        $instance = Instance::ensure(['class' => InstanceArgs::className(), 'foo' => 'foo test'], InstanceArgs::className(), ['bar test', 'baz test']);
         $this->assertSame('foo test', $instance->foo);
         $this->assertSame('bar test', $instance->bar);
+        $this->assertSame('baz test', $instance->baz);
+        $this->assertSame('test', $instance->name);
+
+        $instance = Instance::ensure(['class' => InstanceArgsNull::className(), 'foo' => 'foo test'], InstanceArgsNull::className());
+        $this->assertSame('foo test', $instance->foo);
+        $this->assertNull($instance->bar);
+
+        $instance = Instance::ensure(['class' => InstanceArgsWithoutInterface::className(), 'foo' => 'foo test'], InstanceArgsWithoutInterface::className(), [null]);
+        $this->assertNull($instance->foo);
+        $this->assertNull($instance->bar);
     }
 
     public function testEnsureThrowException()
@@ -51,9 +62,28 @@ class Ensure
     use ObjectTrait;
 }
 
-class InstanceArgs
+class InstanceArgs implements ObjectInterface
 {
+    use ObjectTrait {
+        ObjectTrait::__construct as parentConstruct;
+    }
 
+    public $foo;
+    public $bar;
+    public $baz;
+    public $name;
+
+    public function __construct($bar, $baz = null, $name = 'test', array $config = [])
+    {
+        $this->parentConstruct($config);
+        $this->bar = $bar;
+        $this->baz = $baz;
+        $this->name = $name;
+    }
+}
+
+class InstanceArgsNull implements ObjectInterface
+{
     use ObjectTrait {
         ObjectTrait::__construct as parentConstruct;
     }
@@ -61,10 +91,25 @@ class InstanceArgs
     public $foo;
     public $bar;
 
-    public function __construct($bar, array $config = [])
+    public function __construct($bar = null, array $config = [])
     {
         $this->parentConstruct($config);
         $this->bar = $bar;
     }
+}
 
+class InstanceArgsWithoutInterface
+{
+    use ObjectTrait {
+        ObjectTrait::__construct as parentConstruct;
+    }
+
+    public $foo;
+    public $bar;
+
+    public function __construct($bar = 'bar test', array $config = [])
+    {
+        $this->parentConstruct($config);
+        $this->bar = $bar;
+    }
 }
